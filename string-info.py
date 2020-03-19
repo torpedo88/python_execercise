0829a742ef5b43bdf60c80f3b7c87c52491e37a8
m.avsek94 <m.avsek94@gmail.com>
2020-03-19 08:43:05 +0000

    slicing and indexing a string examples

---
+++ string-info.py
@@ -0,0 +1,21 @@
+my_str = input(str("Please Enter the message ? "))
+l_str = len(my_str)
+m = int (l_str / 2)
+
+f_char = my_str[0].upper()
+l_char = my_str[-1].upper()
+m_char = my_str[m].upper()
+
+even_char = my_str[::2].upper()
+odd_char = my_str[1::2].upper()
+rever_char = my_str[::-1].upper()
+
+print(f"Your input", my_str)
+print(f"First Charcter is", f_char)
+print(f"Last Charcter is", l_char)
+print(f"Middle Charcter is", m_char)
+print(f"Even index Charcters are", even_char)
+print(f"Odd index Charcters are", odd_char)
+print(f"Reversed Charcters are", rever_char)
+
+
