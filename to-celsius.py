b9548e70a4715188be2650abc2802eba3e63fd17
m.avsek94 <m.avsek94@gmail.com>
2020-03-19 08:02:33 +0000

    adding a celsuis converter

---
+++ to-celsius.py
@@ -0,0 +1,5 @@
+f = float(input("Enter the temperature in farenheit to be converted to celsuis ?")) #take an input from the user of temperature in farenheit to be converted to celsuis
+
+c = (f - 32) * 5 / 9
+
+print ("Farenheit", f, "is", "Celsius", c)
