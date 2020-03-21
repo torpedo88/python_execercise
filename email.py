edd228795104f3e552b6c5561cef50626ef5b5a6
m.avsek94 <m.avsek94@gmail.com>
2020-03-21 23:16:14 +0000

    email method added

---
+++ email.py
@@ -0,0 +1,12 @@
+class Mail:
+    def __init__(self, email):
+        self.email = email
+
+    def used_email(self):
+      
+        print("The email address is",  self.email.lower())
+
+email = Mail(str(input(f"Enter the email address :")))
+email.used_email()
+
+assert email == [   ], f"Expected `rev_user` to be ['alice', 'kevin'] but got: {repr(rev_user)}"
